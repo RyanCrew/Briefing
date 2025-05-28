57fbcbfb7cf68087a55da1c8a7d0b66264f9a94a
APALUI <83349874+RyanCrew@users.noreply.github.com>
2025-05-28 23:59:16 +0200

    Update and rename styles.css to App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FlightTable from './FlightTable';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Flight Schedule Analyzer</h1>
+            <FlightTable />
+        </div>
+    );
+}
