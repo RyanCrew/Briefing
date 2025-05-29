8ce95513c4805edc7076981c293d9c8fd515be90
APALUI <83349874+RyanCrew@users.noreply.github.com>
2025-05-29 15:39:13 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/xaiArtifact artifact_id="7205a4f5-5705-49b5-b9ac-5340aa87badc" artifact_version_id="new-tailwind-config" title="tailwind.config.js" contentType="text/javascript">
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: ['./index.html'],
-theme: {
-extend: {},
-},
-plugins: [],
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
