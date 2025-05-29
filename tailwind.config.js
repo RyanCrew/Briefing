6efdc0602221faf952e9ee9d364cf204195b655d
APALUI <83349874+RyanCrew@users.noreply.github.com>
2025-05-29 15:33:07 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-mkdir inflight-report
-cd inflight-report
-npm init -y
-npm install -D tailwindcss
-npx tailwindcss init
+/xaiArtifact artifact_id="7205a4f5-5705-49b5-b9ac-5340aa87badc" artifact_version_id="new-tailwind-config" title="tailwind.config.js" contentType="text/javascript">
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ['./index.html'],
+theme: {
+extend: {},
+},
+plugins: [],
+}
